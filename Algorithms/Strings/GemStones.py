c2837bdf48c246882e21e52912f5a0587bb4a3dc
jagdeesh1656 <jagdeeshxlnc@gmail.com>
2016-09-01 20:56:39 -0500

    GemStones complete.
    No regex.

--- Algorithms/Strings/GemStones.py
+++ Algorithms/Strings/GemStones.py
@@ -1,9 +1,18 @@
 import re
 
-s = ['abcdde','baccd','eeabg']
+s = []
+n = int(input().strip())
+for i in range(n):
+    s.append(input().strip())
 see = [('').join(set(se)) for se in s]
 fs = ('').join(see)
 
-p = re.compile(r'(\w)\1*')
-print re.findall(p,fs)
+gems = {}
+for char in fs:
+	if fs.count(char) == n:
+		gems[char] = n
 
+if len(gems) > 0:
+    print (len(gems))
+else:
+    print ('0')
