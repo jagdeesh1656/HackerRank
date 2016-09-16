663e77ea961b56235cb2f593153044d6460774e6
jagdeesh1656 <jagdeeshxlnc@gmail.com>
2016-09-16 00:41:09 -0500

    New (in-complete) NonDivisibleSubset

---
+++ Algorithms/Implementation/NonDivisibleSubset.py
@@ -0,0 +1,24 @@
+
+n, k = input().strip().split(' ')
+n, k = [int(n), int(k)]
+d = {}
+number = 0
+
+a = [int(_a) for _a in input().strip().split(' ')]
+for _a in a:
+    count = d.setdefault(_a % k, 0)
+    d[_a % k] = count + 1
+
+i = 1
+while 2 * i < k:
+    number = number + max(d.setdefault(i, 0), d.setdefault(k - i, 0))
+    i = i + 1
+
+if 2 * i == k:
+    number = number + 1
+    
+for _value in d.values():
+    if _value == 0:
+        number = number + 1
+
+print (number)
